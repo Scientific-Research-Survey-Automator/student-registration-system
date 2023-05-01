94e4e0d7883313b061d2634fe58b331965b6aa3c
Rishank Karkera <rishankkarkera96@gmail.com>
2023-05-01 02:52:43 -0400

    Merge pull request #7
    * Added exception handling to show error message from procedures.

---
+++ srs-api/src/main/java/edu/binghamton/srs/exception/SrsResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package edu.binghamton.srs.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.UncategorizedSQLException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class SrsResponseEntityExceptionHandler extends ResponseEntityExceptionHandler
+{
+    @ExceptionHandler(UncategorizedSQLException.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(UncategorizedSQLException ex, WebRequest request) {
+        //creating exception response structure
+        ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getSQLException().getMessage(), request.getDescription(false));
+        //returning exception structure and specific status
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
