ea5a444e7984e0665c38e5428eec88c4f6b253a8
Rishank Karkera <rishankkarkera96@gmail.com>
2023-05-03 20:19:46 -0400

    Added cors mapping for react, modified Student consturctor and refactored minor bug for student enrollment procedure. (#9)

--- srs-api/src/main/java/edu/binghamton/srs/model/StudentDetails.java
+++ srs-api/src/main/java/edu/binghamton/srs/model/StudentDetails.java
@@ -2,11 +2,13 @@ package edu.binghamton.srs.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @SuperBuilder
+@NoArgsConstructor
 public class StudentDetails {
     private String bNumber;
     private String firstName;
